7de6113414b1513b0e86c89b1d0a2c543c0923fb
Alexey A. Leonov <aaleonov@yandex.ru>
2020-03-12 03:06:49 +0300

    Added canceled messages

--- successrate.ps1
+++ successrate.ps1
@@ -11,94 +11,131 @@ if (-not $Path) {
 "========== AUDIT ============="
 
 $auditsSuccess = ($log | Select-String GET_AUDIT | Select-String downloaded).Count
-"Successful:`t`t" + $auditsSuccess
 
 $auditsFailed = ($log | Select-String GET_AUDIT | Select-String failed | Select-String open -NotMatch).Count
-"Recoverable failed:`t" + $auditsFailed
 
 $auditsFailedCritical = ($log | Select-String GET_AUDIT | Select-String failed | Select-String open).Count
-"Unrecoverable failed:`t" + $auditsFailedCritical
 
 if (($auditsSuccess + $auditsFailed + $auditsFailedCritical) -ge 1) {
-    $auditsRateMin = $auditsSuccess / ($auditsSuccess + $auditsFailed + $auditsFailedCritical) * 100
+    $audits_failed_ratio = $auditsFailed / ($auditsSuccess + $auditsFailed + $auditsFailedCritical) * 100
+    $audits_critical_ratio = $auditsFailedCritical / ($auditsSuccess + $auditsFailed + $auditsFailedCritical) * 100
+    $audits_success_ratio = $auditsSuccess / ($auditsSuccess + $auditsFailed + $auditsFailedCritical) * 100
 } else {
-    $auditsRateMin = 0.00
+    $audits_failed_ratio = 0.00
+    $audits_critical_ratio = 0.00
+    $audits_success_ratio = 0.00
 }
-"Success Min:`t`t" + $auditsRateMin + "%"
 
-if (($auditsSuccess + $auditsFailedCritical) -ge 1) {
-    $auditsRateMax = $auditsSuccess / ($auditsSuccess + $auditsFailedCritical) * 100
-} else {
-    $auditsRateMax = 0.00
-}
-"Success Max:`t`t" + $auditsRateMax + "%"
+"Successful:`t`t" + $auditsSuccess
+"Recoverable failed:`t" + $auditsFailed
+"Recoverable Fail Rate:`t{0:N}%" -f $audits_failed_ratio
+"Critically failed:`t" + $auditsFailedCritical
+"Critical Fail Rate:`t{0:N}%" -f $audits_critical_ratio
+"Success Rate:`t`t{0:N}%" -f $audits_success_ratio
 
 "========== DOWNLOAD =========="
 
 $dl_success = ($log | Select-String '"GET"' | Select-String downloaded).Count
-"Successful:`t`t" + $dl_success
+
+$dl_canceled = ($log | Select-String '"GET"' | Select-String canceled).Count
 
 $dl_failed = ($log | Select-String '"GET"' | Select-String failed).Count
-"Failed:`t`t`t" + $dl_failed
 
-if (($dl_success + $dl_failed) -ge 1) {
-    $dl_ratio = $dl_success / ($dl_success + $dl_failed) * 100
+if (($dl_success + $dl_failed + $dl_canceled) -ge 1) {
+    $dl_failed_ratio = $dl_failed / ($dl_success + $dl_failed + $dl_canceled) * 100
+    $dl_canceled_ratio = $dl_canceled / ($dl_success + $dl_failed + $dl_canceled) * 100
+    $dl_ratio = $dl_success / ($dl_success + $dl_failed + $dl_canceled) * 100
 } else {
+    $dl_failed_ratio = 0.00
+    $dl_canceled_ratio = 0.00
     $dl_ratio = 0.00
 }
-"Success Rate:`t`t" + $dl_ratio
+
+"Successful:`t`t" + $dl_success
+"Failed:`t`t`t" + $dl_failed
+"Fail Rate:`t`t{0:N}%" -f $dl_failed_ratio
+"Canceled:`t`t" + $dl_canceled
+"Cancel Rate:`t`t{0:N}%" -f $dl_canceled_ratio
+"Success Rate:`t`t{0:N}%" -f $dl_ratio
 
 "========== UPLOAD ============"
 
 $put_success = ($log | Select-String '"PUT"' | Select-String uploaded).Count
-"Successful:`t`t" + $put_success
 
 $put_rejected = ($log | Select-String '"PUT"' | Select-String rejected).Count
-"Rejected:`t`t" + $put_rejected
+
+$put_canceled = ($log | Select-String '"PUT"' | Select-String canceled).Count
 
 $put_failed = ($log | Select-String '"PUT"' | Select-String failed).Count
-"Failed:`t`t`t" + $put_failed
 
-if (($put_success + $put_rejected) -ge 1) {
-    $put_accept_ratio = $put_success / ($put_success + $put_rejected) * 100
+if (($put_success + $put_rejected + $put_failed + $put_canceled) -ge 1) {
+    $put_failed_ratio = $put_failed / ($put_success + $put_rejected + $put_failed + $put_canceled) * 100
+    $put_canceled_ratio = $put_canceled / ($put_success + $put_rejected + $put_failed + $put_canceled) * 100
+    $put_accept_ratio = ($put_success + $put_canceled + $put_failed) / ($put_success + $put_rejected + $put_failed + $put_canceled) * 100
+    $put_ratio = $put_success / ($put_success + $put_rejected + $put_failed + $put_canceled) * 100
 } else {
+    $put_failed_ratio = 0.00
+    $put_canceled_ratio = 0.00
     $put_accept_ratio = 0.00
-}
-"Acceptance Rate:`t" + $put_accept_ratio
-
-if (($put_success + $put_failed) -ge 1) {
-    $put_ratio = $put_success / ($put_success + $put_failed) * 100
-} else {
     $put_ratio = 0.00
 }
-"Success Rate:`t`t" + $put_ratio
+
+"Successful:`t`t" + $put_success
+"Rejected:`t`t" + $put_rejected
+"Acceptance Rate:`t{0:N}%" -f $put_accept_ratio
+"---------- accepted ----------"
+"Failed:`t`t`t" + $put_failed
+"Fail Rate:`t`t{0:N}%" -f $put_failed_ratio
+"Canceled:`t`t" + $put_canceled
+"Cancel Rate:`t`t{0:N}%" -f $put_canceled_ratio
+"Success Rate:`t`t{0:N}%" -f $put_ratio
 
 "========== REPAIR DOWNLOAD ==="
 
 $get_repair_success = ($log | Select-String GET_REPAIR | Select-String downloaded).Count
-"Successful:`t`t" + $get_repair_success
+
+$get_repair_canceled = ($log | Select-String GET_REPAIR | Select-String canceled).Count
 
 $get_repair_failed = ($log | Select-String GET_REPAIR | Select-String failed).Count
-"Failed:`t`t`t" + $get_repair_failed
 
-if (($get_repair_success + $get_repair_failed) -ge 1) {
-    $get_repair_ratio = $get_repair_success / ($get_repair_success + $get_repair_failed) * 100
+if (($get_repair_success + $get_repair_failed + $get_repair_canceled) -ge 1) {
+    $get_repair_faled_ratio = $get_repair_failed / ($get_repair_success + $get_repair_failed + $get_repair_canceled) * 100
+    $get_repair_canceled_ratio = $get_repair_canceled / ($get_repair_success + $get_repair_failed + $get_repair_canceled) * 100
+    $get_repair_ratio = $get_repair_success / ($get_repair_success + $get_repair_failed + $get_repair_canceled) * 100
 } else {
+    $get_repair_failed_ratio = 0.00
+    $get_repair_canceled_ratio = 0.00
     $get_repair_ratio = 0.00
 }
-"Success Rate:`t`t" + $get_repair_ratio
+
+"Successful:`t`t" + $get_repair_success
+"Failed:`t`t`t" + $get_repair_failed
+"Fail Rate:`t`t{0:N}%" -f $get_repair_failed_ratio
+"Canceled:`t`t" + $get_repair_canceled
+"Cancel Rate:`t`t{0:N}%" -f $get_repair_canceled_ratio
+"Success Rate:`t`t{0:N}%" -f $get_repair_ratio
 
 "========== REPAIR UPLOAD ====="
 
 $put_repair_success = ($log | Select-String PUT_REPAIR | Select-String uploaded).Count
-"Successful:`t`t" + $put_repair_success
+
+$put_repair_canceled = ($log | Select-String PUT_REPAIR | Select-String canceled).Count
 
 $put_repair_failed = ($log | Select-String PUT_REPAIR | Select-String failed).Count
-"Failed:`t`t`t" + $put_repair_failed
 
-if (($put_repair_success + $put_repair_failed) -ge 1) {
-    $put_repair_ratio = $put_repair_success / ($put_repair_success + $put_repair_failed) * 100
+if (($put_repair_success + $put_repair_failed + $put_repair_canceled) -ge 1) {
+    $put_repair_failed_ratio = $put_repair_failed / ($put_repair_success + $put_repair_failed + $put_repair_canceled) * 100
+    $put_repair_canceled_ratio = $put_repair_canceled / ($put_repair_success + $put_repair_failed + $put_repair_canceled) * 100
+    $put_repair_ratio = $put_repair_success / ($put_repair_success + $put_repair_failed + $put_repair_canceled) * 100
 } else {
+    $put_repair_failed_ratio = 0.00
+    $put_repair_canceled_ratio = 0.00
     $put_repair_ratio = 0.00
 }
-"Success Rate:`t`t" + $put_repair_ratio
+
+"Successful:`t`t" + $put_repair_success
+"Failed:`t`t`t" + $put_repair_failed
+"Fail Rate:`t`t{0:N}%" -f $put_repair_failed_ratio
+"Canceled:`t`t" + $put_repair_canceled
+"Cancel Rate:`t`t{0:N}%" -f $put_repair_canceled_ratio
+"Success Rate:`t`t{0:N}%" -f $put_repair_ratio
